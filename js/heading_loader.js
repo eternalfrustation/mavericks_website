f1c3686e39ff68f863bf554b00533f4c1056099e
Sandeep Kumar <sandeepqmar05@gmail.com>
2023-05-14 14:22:40 +0530

    More fragmentation in frontend

--- js/heading_loader.js
+++ js/heading_loader.js
@@ -5,13 +5,23 @@ this.addEventListener("load", async function () {
   }
 }, false);
 
+
+
 loadEvents.push(
   async () => {
-    await fetch("/heading.html").then((resp) => {
+    await fetch("/heading").then((resp) => {
       return resp.text();
     }).then((html) => {
-      const body = document.getElementsByTagName("body")[0];
-      body.innerHTML = html + body.innerHTML;
+      const frag = document.createRange().createContextualFragment(html);
+      for (const e of frag.getElementById("topnav").children) {
+        if (e.href == document.URL) {
+	  e.href = "#";
+	  e.classList.add("active");
+	  break;
+        }
+      }
+      const header = document.getElementsByClassName("header")[0];
+      header.prepend(frag);
     });
   },
 );
